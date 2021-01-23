231ad31c1ccbfcc96415b409885cddff46bdd73c
Victoire Sebu <vitechsebu@gmail.com>
2021-01-23 07:46:48 +0100

    add connection with db

---
+++ models/dbconfig.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/backend-node",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (!err) console.log("Mongodb connected");
+        else console.log("Connection error: " + err);
+    }
+)
